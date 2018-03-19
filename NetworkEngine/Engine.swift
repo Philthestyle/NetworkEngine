90537dc8c5e0244fdf06e283c2307f3650eb4cab
Faustin Veyssiere <faustin.veyssiere@gmail.com>
2018-03-19 13:57:04 +0100

    Engine class init done

---
+++ NetworkEngine/Engine.swift
@@ -0,0 +1,23 @@
+//
+//  Engine.swift
+//  NetworkEngine
+//
+//  Created by Faustin Veyssiere on 19/03/2018.
+//  Copyright © 2018 Faustin Veyssiere. All rights reserved.
+//
+
+import Foundation
+
+
+
+
+class Engine {
+    
+    var route: String
+    
+    init(route: String = "www.google.fr") {
+        self.route = route 
+    }
+    
+    
+}
